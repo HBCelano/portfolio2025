55ac896bab616e9971c20d9fcf093cbc1d956415
HBCelano <benjaminncelano@gmail.com>
2025-04-07 20:52:03 -0300

    push con ssh

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -2,7 +2,6 @@ import { Typography } from "@mui/material";
 
 const Skills = () => <>
     <Typography variant="h2" className="text-center">Habilidades</Typography>
-    <Typography variant="h2" className="text-center">Otro cambio para hacer pull request en github y luego pull desde local.</Typography>
 </>;
 
 export default Skills;
