68ed0aed8b251b5ca1cc53ae6f41738f9a5fdda3
HBCelano <benjaminncelano@gmail.com>
2025-04-19 20:42:10 -0300

    Modify skills

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -2,7 +2,7 @@ import { Typography } from "@mui/material";
 
 const Skills = () => <>
     <Typography variant="h2" className="text-center">Habilidades</Typography>
-    <Typography variant="h2" className="text-center">Subtítulo</Typography>
+    {/* <Typography variant="h2" className="text-center">Subtítulo</Typography> */}
 </>;
 
 export default Skills;
