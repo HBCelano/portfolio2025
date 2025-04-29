3141173a2f2822071158b842075991dae0d892e0
HBCelano <benjaminncelano@gmail.com>
2025-04-28 21:20:09 -0300

    Avanza Skills

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -1,32 +1,88 @@
+'use client';
+
+import Image from 'next/image';
 import Box from "@mui/material/Box";
 import Typography from '@mui/material/Typography';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/autoplay';
 
-const SkillCard = ({ imgSrc, title }: { imgSrc: string, title: string, desc?: string }) => (
-    <Card sx={{ maxWidth: 250 }}>
-        <CardMedia
-            component="img"
-            alt="Imagen de Habilidad"
-            height="50"
-            image={imgSrc}
+const skills = [
+    {
+        img: '/img/skills/html5.svg',
+        name: 'HTML'
+    },
+    {
+        img: '/img/skills/css.svg',
+        name: 'CSS'
+    },
+    {
+        img: '/img/skills/bootstrap.svg',
+        name: 'Bootstrap'
+    },
+    {
+        img: '/img/skills/js.svg',
+        name: 'JavaScript'
+    },
+    {
+        img: '/img/skills/jquery.svg',
+        name: 'jQuery'
+    },
+    {
+        img: '/img/skills/react.svg',
+        name: 'React'
+    },
+    {
+        img: '/img/skills/nextjs.svg',
+        name: 'NextJS'
+    },
+    {
+        img: '/img/skills/git.svg',
+        name: 'Git'
+    },
+    {
+        img: '/img/skills/github.svg',
+        name: 'GitHub'
+    },
+    {
+        img: '/img/skills/python.svg',
+        name: 'Python'
+    },
+    {
+        img: '/img/skills/php.svg',
+        name: 'PHP'
+    },
+    {
+        img: '/img/skills/mysql.svg',
+        name: 'MySQL'
+    },
+    {
+        img: '/img/skills/postgresql.svg',
+        name: 'PostgreSQL'
+    }
+];
+
+const SkillCard = ({ imgSrc, title, desc }: { imgSrc: string, title: string, desc?: string }) => (
+    <Box
+        component='section'
+        sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center'
+        }}
+    >
+        <Image
+            src={imgSrc}
+            alt='Imagen de Habilidad'
+            width={120}
+            height={120}
         />
-        <CardContent>
-            <Typography gutterBottom variant="h5" component="h5">
-                {title}
-            </Typography>
-            <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-                Lizards are a widespread group of squamate reptiles, with over 6,000
-                species, ranging across all continents except Antarctica
-            </Typography>
-        </CardContent>
-        <CardActions>
-            <Button size="small">Leer más</Button>
-        </CardActions>
-    </Card>
+        <Typography component='h4' variant='h4' className='text-center' mt={2} fontWeight={200}>{title}</Typography>
+        {desc &&
+            <Typography component='span' variant='body2' color='textSecondary' className='text-center' my={2}>{desc}</Typography>
+        }
+    </Box>
 );
 
 const Skills = () => (
@@ -35,24 +91,51 @@ const Skills = () => (
         <Box
             component='section'
             sx={{
-                display: 'flex',
-                justifyContent: 'center',
-                flexWrap: 'wrap',
-                gap: 5
+                // display: 'flex',
+                // justifyContent: 'center',
+                // flexWrap: 'wrap',
+                // gap: 10,
+                px: 10,
+                mt: 6
             }}
         >
-            <SkillCard
-                imgSrc="/img/skills/html.png"
-                title="HTML"
-            />
-            <SkillCard
-                imgSrc="/img/skills/css.png"
-                title="CSS"
-            />
-            <SkillCard
-                imgSrc="/img/skills/bootstrap.png"
-                title="Bootstrap"
-            />
+            <Swiper
+                modules={[Autoplay]}
+                autoplay={{ delay: 1000 }}
+                loop
+                // spaceBetween={5}
+                breakpoints={{
+                    0: {
+                        slidesPerView: 1
+                    },
+                    300: {
+                        slidesPerView: 2
+                    },
+                    600: {
+                        slidesPerView: 3
+                    },
+                    900: {
+                        slidesPerView: 4
+                    },
+                    1200: {
+                        slidesPerView: 6
+                    }
+                }}
+            // style={{ height: '100%' }}
+            >
+                {
+                    skills.map((skill, index) => (
+                        <SwiperSlide
+                            key={index}
+                        >
+                            <SkillCard
+                                imgSrc={skill.img}
+                                title={skill.name}
+                            />
+                        </SwiperSlide>
+                    ))
+                }
+            </Swiper>
         </Box>
     </>
 );
