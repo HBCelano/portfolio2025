d2a92c87167956835faeaa60c5e71d0a802f1d7c
HBCelano <benjaminncelano@gmail.com>
2025-05-07 17:22:38 -0300

    Init feat/contact

--- src/presentation/contact/CustomForm.tsx
+++ src/presentation/contact/CustomForm.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import Image from 'next/Image';
+import Image from 'next/image';
 import { useTheme, styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
