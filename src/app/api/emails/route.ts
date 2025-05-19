6af0221ff011b13da20a92792f38e02d8bfc61d8
HBCelano <benjaminncelano@gmail.com>
2025-05-19 15:23:49 -0300

    email contact

--- src/app/api/emails/route.ts
+++ src/app/api/emails/route.ts
@@ -42,32 +42,74 @@ export async function POST(request: NextRequest) {
                     pass: process.env.EMAIL_PASSWORD_SECRET
                 }
             });
+            const date = new Date().getFullYear();
             const mailOptions = {
                 from: process.env.EMAIL_ADDRESS_SECRET,
                 to: process.env.EMAIL_ADDRESS_TO_SECRET,
                 subject: `Nuevo mensaje de ${formData.get('name')}`,
                 html: `
-                    <div style="width: 100%; max-width: 600px; margin: 20px auto; background-color: #ffffff; border-radius: 8px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); overflow: hidden;">
-                        <div style="display: flex; align-items: center; background-color: #3498db; color: white; padding: 20px;">
-                            <img src="https://hbcelano.vercel.app/img/hc-light.svg" alt="Logo" style="width: 50px; height: 50px; border-radius: 50%; margin-right: 15px;">
-                            <h1 style="font-size: 24px; margin: 0;">HBCelano</h1>
-                        </div>
-                        <div style="padding: 20px;">
-                            <h2 style="font-size: 18px; margin-bottom: 10px;">Datos del Usuario:</h2>
-                            <ul style="list-style-type: none; padding: 0;">
-                            <li style="background-color: #f9f9f9; margin-bottom: 10px; padding: 10px; border-radius: 5px; box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1);">
-                                <p style="margin: 0;"><strong>Nombre: </strong>${formData.get('name')}</p>
-                            </li>
-                            <li style="background-color: #f9f9f9; margin-bottom: 10px; padding: 10px; border-radius: 5px; box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1);">
-                                <p style="margin: 0;"><strong>E-mail: </strong>${formData.get('email')}</p>
-                            </li>
-                            <li style="background-color: #f9f9f9; margin-bottom: 10px; padding: 10px; border-radius: 5px; box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1);">
-                                <p style="margin: 0;"><strong>Mensaje: </strong>${formData.get('message')}</p>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            `,
+                    <!DOCTYPE html>
+                    <html lang="es">
+                        <head>
+                            <meta charset="UTF-8">
+                            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                            <title>Email</title>
+                        </head>
+                        <body>
+                            <table cellpadding="0" cellspacing="0" border="0" width="100%" style="width: 100%; max-width: 600px; border: none; border-radius: 15px; margin: 40px auto; background-color: #90caf9;">
+                                <thead>
+                                    <tr>
+                                        <th valign="middle" align="left" style="vertical-align: middle; text-align: left; padding: 20px;">
+                                            <table cellpadding="0" cellspacing="0" border="0" style="border: none;">
+                                                <thead>
+                                                    <tr>
+                                                        <th valign="middle" style="vertical-align: middle;">
+                                                            <img src="https://hbcelano.vercel.app/img/hc-light.png" alt="Logo" width="50" height="50" style="display: block; border-radius: 50%;">
+                                                        </th>
+                                                        <th width="8"></th>
+                                                        <th valign="middle" style="vertical-align: middle;">
+                                                            <h1 style="font-family: Arial,Helvetica,sans-serif; font-size: 1.8rem; font-weight: 400; color: black; margin: 0;">HBCelano</h1>
+                                                        </th>
+                                                    </tr>
+                                                </thead>
+                                            </table>
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody style="background-color: #272727;">
+                                    <tr>
+                                        <td valign="middle" colspan="100%" style="vertical-align: middle;">
+                                            <h2 style="font-family: Arial,Helvetica,sans-serif; font-size: 1.4rem; font-weight: 300; text-align: center; color: white; padding: .8rem;">DATOS DEL USUARIO</h2>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td valign="middle" style="vertical-align: middle; font-family: Arial,Helvetica,sans-serif; font-size: 1rem; color: white; padding: .6rem 1.5rem;"><strong>🙋‍♂️ NOMBRE: </strong>${formData.get('name')}
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td valign="middle" style="vertical-align: middle; font-family: Arial,Helvetica,sans-serif; font-size: 1rem; color: white; padding: .6rem 1.5rem;"><strong>📌 E-MAIL: </strong>${formData.get('email')}
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td valign="middle" style="vertical-align: middle; font-family: Arial,Helvetica,sans-serif; font-size: 1rem; color: white; padding: .6rem 1.5rem; padding-bottom: 3rem;"><strong>📧 MENSAJE: </strong>${formData.get('message')}
+                                        </td>
+                                    </tr>
+                                </tbody>
+                                <tfoot style="background-color: #121212; border-bottom-left-radius: 15px;border-bottom-right-radius: 15px;">
+                                    <tr style="border-bottom-left-radius: inherit; border-bottom-right-radius:inherit;">
+                                        <td valign="middle" colspan="100%" style="vertical-align: middle;border-bottom-left-radius: inherit; border-bottom-right-radius: inherit;">
+                                            <h5 style="font-family: Arial,Helvetica,sans-serif; font-size: 1rem;font-weight: 300; text-align: center; color: white; margin: .8rem;">
+                                                <span>
+                                                    ${date} &copy; HBCelano
+                                                </span>
+                                            </h5>
+                                        </td>
+                                    </tr>
+                                </tfoot>
+                            </table>
+                        </body>
+                    </html>                        
+                `
             };
             await transporter.sendMail(mailOptions);
             return new NextResponse(undefined, { status: 200 });
